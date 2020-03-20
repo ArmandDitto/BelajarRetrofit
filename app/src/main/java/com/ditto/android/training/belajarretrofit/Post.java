f23e8eb113522dd9cbcae3acdc5395b8e7f61a70
aturridwan07 <rb.aturridwan@gmail.com>
2020-03-20 11:11:47 +0700

    Get Data from JsonPlaceHolderApi

---
+++ app/src/main/java/com/ditto/android/training/belajarretrofit/Post.java
@@ -0,0 +1,24 @@
+package com.ditto.android.training.belajarretrofit;
+
+public class Post {
+    public int userId;
+    public int id;
+    public String title;
+    public String body;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
