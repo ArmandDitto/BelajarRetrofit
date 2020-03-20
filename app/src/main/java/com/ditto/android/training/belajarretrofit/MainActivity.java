2ec1e8fa629d9fc575054e1b3ed3e2814467f309
aturridwan07 <rb.aturridwan@gmail.com>
2020-03-20 10:16:35 +0700

    First Commit

---
+++ app/src/main/java/com/ditto/android/training/belajarretrofit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ditto.android.training.belajarretrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
